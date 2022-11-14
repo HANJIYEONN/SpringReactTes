7bd050d4b4423d1f050d7292e41aa19953d2e165
HANJIYEONN <onedelay1@naver.com>
2022-11-14 13:40:04 +0900

    useState 와 컴포넌트

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -5,13 +5,18 @@ import './App.css';
 function App() {
 
 //let name = "eggRollMaster";
+//let [name, setName] = useState('egg');
+//let num = 1;
+let [num, setNum] = useState(1);
 
-let [name, setName] = useState('egg');
+
+setTimeout(()=> {setNum(num = num+1)},1000);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>{ name }</div>
+        <div>{ num }</div>
+        {<button onClick = {() => {setNum(num += 1)}}>버튼</button> }
       </header>
     </div>
   );
