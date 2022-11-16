6c2b83779b7baabfae93b4e3d501b2da294e826e
HANJIYEONN <onedelay1@naver.com>
2022-11-17 00:00:44 +0900

    input 태그

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -1,49 +1,30 @@
 import React, {useState} from 'react';
-import './App.css';
-
-const RecordForm = ({numList, setNumList}) => {
-
-  const [num, setNum] = useState(0); 
-
-  return (
-  <div>
-    <div>현재 숫자 : {num}</div>
-    <button onClick={() => setNum(num +1)}>숫자 증가</button>
-    <button onClick={() => setNum(num -1)}>숫자 감소</button>
-    <button onClick={() => setNum(0)}>초기화</button>
-    <hr/>
-    <button onClick={() => setNumList([...numList, num ])}>숫자 기록</button>
-    <button onClick={() => setNumList([])}>숫자 초기화</button>
-    
-  </div>
-  )
-}
-
-const RecordList = ({numList}) => {
-
-  return (
-    <div>
-      <h2>기록된 숫자</h2>
-      {numList.length > 0 ? 
-        <div>{numList.map((map)=> <li>{map}</li>)}</div> 
-        : <div>기록 없음</div>}
-    </div>
-  )
-}
 
 const App = () => {
 
-  const [numList, setNumList] = useState([]);
+  const [text,setText] = useState("");
+  const [edit,setEdit] = useState(false);
+
+  let content = <div>
+    {text}<button onClick={() => setEdit(true)}>수정</button>
+    </div>
+
+  if(edit) {
+    content = <div>
+    <input type="text" 
+      value={text}
+      onChange={(e)=>{
+        setText(e.target.value);
+      }}
+    />
+    <button onClick={() => setEdit(false)}>수정</button>
+  </div>
+  }
 
   return (
-    <div style={{margin : "40px auto"
-                , width : "800px"
-                , textAlign :  "center"
-                }}>
-      <RecordForm numList={numList} setNumList={setNumList}/>
-      <RecordList numList={numList} />
-              
-    </div>
+    <>
+    {content}
+    </>
   )
 }
 
