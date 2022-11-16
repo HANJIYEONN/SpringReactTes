ccf1d57fd12654118f9d44d01bc1384c83c4b7ee
HANJIYEONN <onedelay1@naver.com>
2022-11-16 14:00:09 +0900

    함수 분리

--- src/main/frontreact/src/App.js
+++ src/main/frontreact/src/App.js
@@ -1,32 +1,50 @@
 import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const RecordForm = ({numList, setNumList}) => {
 
-const [num, setNum] = useState(0      );
-
-const [numList, setNumList] = useState([]);
-
-function numRecoding(){
-  setNumList([...numList, num]); 
-
-  setNum(0)
-}
+  const [num, setNum] = useState(0); 
 
   return (
-    <div className="App">
-      <div>현재 숫자 : { num } </div>
-      <button onClick={()=> {setNum(num+1)}}>숫자 증가</button>
-      <button onClick={()=> {setNum(num-1)}}>숫자 감소</button>
-      <button onClick={numRecoding}>숫자 기록</button>
-      <h1 >저장된 숫자</h1>
-      <ul>
-        {numList.map((num) => (
-          <li>{num}</li>
-        ))}
-      </ul>
+  <div>
+    <div>현재 숫자 : {num}</div>
+    <button onClick={() => setNum(num +1)}>숫자 증가</button>
+    <button onClick={() => setNum(num -1)}>숫자 감소</button>
+    <button onClick={() => setNum(0)}>초기화</button>
+    <hr/>
+    <button onClick={() => setNumList([...numList, num ])}>숫자 기록</button>
+    <button onClick={() => setNumList([])}>숫자 초기화</button>
+    
+  </div>
+  )
+}
+
+const RecordList = ({numList}) => {
+
+  return (
+    <div>
+      <h2>기록된 숫자</h2>
+      {numList.length > 0 ? 
+        <div>{numList.map((map)=> <li>{map}</li>)}</div> 
+        : <div>기록 없음</div>}
     </div>
-  );
+  )
+}
+
+const App = () => {
+
+  const [numList, setNumList] = useState([]);
+
+  return (
+    <div style={{margin : "40px auto"
+                , width : "800px"
+                , textAlign :  "center"
+                }}>
+      <RecordForm numList={numList} setNumList={setNumList}/>
+      <RecordList numList={numList} />
+              
+    </div>
+  )
 }
 
 export default App;
